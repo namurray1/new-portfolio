28e68e4c94bb4035e96a345fa964d4ba30e6942b
Alex Murray <namurray1@gmail.com>
2018-02-08 19:19:33 -0500

    updating CSS properties for get to top button

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ $(function(){
     if (this.hash !== "") {
       event.preventDefault();
       var hash = this.hash;
-      $("#about me").animate({
+      $("#about me, #projects, #contact me").animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
         window.location.hash = hash;
@@ -14,24 +14,26 @@ $(function(){
   
   
 });
+//make sure to test this. For now we will comment it out
 
- function onClick(element) {
-     document.getElementById("img01").src = element.src;
-     document.getElementById("modal01").style.display = "block";
-}
+//  function onClick(element) {
+//      document.getElementById("img01").src = element.src;
+//      document.getElementById("modal01").style.display = "block";
+// }
 
- window.onscroll = function() {
-   scrollFunction();
-  };
+//  window.onscroll = function() {
+//    scrollFunction();
+//   };
 
- function scrollFunction() {
-   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-     document.getElementById("myBtn").style.display = "block";
-   } else {
-     document.getElementById("myBtn").style.display = "none";
-   }
- }
+//  function scrollFunction() {
+//    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//      document.getElementById("myBtn").style.display = "block";
+//    } else {
+//      document.getElementById("myBtn").style.display = "none";
+//    }
+//  }
 
- function topFunction() {
-   document.body.scrollTop = 0;   document.documentElement.scrollTop = 0;
- }
+//  function topFunction() {
+//    document.body.scrollTop = 0;   
+//    document.documentElement.scrollTop = 0;
+//  }
