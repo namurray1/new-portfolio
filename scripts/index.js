f8de5296c637b8f0c195b43473481d17b20ccc8e
Alex Murray <namurray1@gmail.com>
2018-02-11 02:07:30 -0500

    made some change to document. Added parallax

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 
-  $('a[href^="#"]').click(function(e){ // where e = event
-    e.preventDefault();
+  
 
-    var hash =  $($(this).attr('href')).offset().top;
+  // $('a[href^="#"]').click(function(e){ // where e = event
+  //   e.preventDefault();
 
-    $('html, body').animate({
-      scrollTop: position
-    }, 1000, 'linear');
-  });
+  //   var hash =  $($(this).attr('href')).offset().top;
+
+  //   $('html, body').animate({
+  //     scrollTop: position
+  //   }, 1000, 'linear');
+  // });
 
   // $("a[href*='#']").on("click", function(){ // you can use the anchor tag or the reference to the anchors as shown "a[href^='#']".
   //   var target = $(this.hash);
@@ -31,29 +33,41 @@ $(document).ready(function(){
   
 });
 
+//Change style of navbar
+window.onscroll = function() {myFunction();};
+function myFunction() {
+  var navbar = document.getElementById("myNavbar");
+  if(document.body.scrollTop > 100) || document.documentElement.scrollTop > 100) {
+    navbar.className = "w3-bar" + "w3-card" + "w3-animate-top" + " w3-white";
+  } else {
+    navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+  }
+}
+
+
 
 
 // Model for image grid
 
-function onClick(element) { 
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
-}
-// scroll function for "Go to Top" button
+// function onClick(element) { 
+//   document.getElementById("img01").src = element.src;
+//   document.getElementById("modal01").style.display = "block";
+// }
+// // scroll function for "Go to Top" button
 
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("myBtn").style.display = "block";
+//   } else {
+//     document.getElementById("myBtn").style.display = "none";
+//   }
+// }
 
-function topFunction() { 
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function topFunction() { 
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
