8880b376de36122294c5a171496fbd89df19ddd9
Alex Murray <namurray1@gmail.com>
2018-02-11 05:07:40 -0500

    updated code and deleted several images

--- scripts/index.js
+++ scripts/index.js
@@ -34,17 +34,30 @@ $(document).ready(function(){
 });
 
 //Change style of navbar
-window.onscroll = function() {myFunction();};
+window.onscroll = function() {
+  myFunction();
+};
 function myFunction() {
   var navbar = document.getElementById("myNavbar");
-  if(document.body.scrollTop > 100) || document.documentElement.scrollTop > 100) {
-    navbar.className = "w3-bar" + "w3-card" + "w3-animate-top" + " w3-white";
+  if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    navbar.className = "w3-bar" + "w3-card" + " w3-animate-top" + " w3-white";
   } else {
     navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
   }
 }
 
+// You need to fix how the navbar changes color and opacity when the scroll begins here!!!
 
+
+// function for opening menu on small screens
+function toggleFunction() {
+  var x = document.getElementById("navDemo");
+  if(x.className.indexOf("w3-show") === -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
 
 
 // Model for image grid
